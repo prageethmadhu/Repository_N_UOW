e1152c57cbdef4427171e96afc9c480e3081783a
unknown <prageeth.madhu@gmail.com>
2025-02-19 10:07:26 +0530

    added git ignore

--- obj/Debug/net9.0/new-api-layout.AssemblyInfo.cs
+++ obj/Debug/net9.0/new-api-layout.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("new-api-layout")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4ba0744628e40bd1c9e59a525f287449c02ce55b")]
 [assembly: System.Reflection.AssemblyProductAttribute("new-api-layout")]
 [assembly: System.Reflection.AssemblyTitleAttribute("new-api-layout")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
